a958d03a49b03804f25589aaa115a72a3754fb63
N0mena <alida.rajaonarivelo@gmail.com>
2026-07-13 09:00:07 +0300

    feat: ImageService added

---
+++ src/main/java/hei/school/nmn/endpoint/event/model/SubmissionEmailRequested.java
@@ -0,0 +1,25 @@
+package hei.school.nmn.endpoint.event.model;
+
+import java.time.Duration;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class SubmissionEmailRequested extends PojaEvent {
+  private String email;
+  private String bucketKey;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(45);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
